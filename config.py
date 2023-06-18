51edc8fd9282689566625e5bc5c2d57bacd9a745
25th-Night <brown_eyed87@naver.com>
2023-06-18 10:28:27 +0900

    Fix TestSettings

--- config.py
+++ config.py
@@ -39,6 +39,7 @@ class DevSettings(Settings):
 
 
 class TestSettings(Settings):
+    TEST_MODE = True
     DB_URL = "mysql+pymysql://admin:1234@localhost:3306/imizi_api?charset=utf8mb4"
     DB_POOL_SIZE = 1
     DB_MAX_OVERFLOW = 0
